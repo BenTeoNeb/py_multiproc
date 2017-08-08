9ccc076c3234513915aa5a17f2e90abe58a451c3
Benjamin Farcy <benjamin.farcy@cerfacs.fr>
2017-08-08 17:24:05 +0200

    Cleaning code

--- pool.py
+++ pool.py
@@ -1,3 +1,7 @@
+"""
+Experimenting with multiprocessing in python
+"""
+
 import time
 import os
 import sys
@@ -7,22 +11,25 @@ from Queue import Queue
 from multiprocessing import Process, Pool, Lock
 import multiprocessing
 
-def doWork(N, pid, lock):
+
+def do_work(complexity, pid, lock):
     """ Do something time consuming and locking at the end """
 
-    startTime = time.time()
-    result = 0 
-    print "--> Process:", pid," ospid:",os.getpid(), " START"
+    start_time = time.time()
+    result = 0
+    print "--> Process:", pid, " ospid:", os.getpid(), " START"
     # Do something time consuming
-    for i in range(N):
-       result += 1*(math.log(3.14**(1.2)/14.3**(0.9))/math.exp(0.314**(1.314)/1.13))
+    for i in range(complexity):
+        result += 1 * (math.log(3.14**(1.2) / 14.3**(0.9)) /
+                       math.exp(0.314**(1.314) / 1.13))
     # when finished do something locking
     # This means that only one worker can have the lock at the same time
     lock.acquire()
-    print "--> Process:", pid," ospid:",os.getpid(), " LOCKING"
+    print "--> Process:", pid, " ospid:", os.getpid(), " LOCKING"
     time.sleep(1)
-    print "--> Process:", pid," ospid:",os.getpid(), " N=", N, " time:", time.time()-startTime, " END"
+    print "--> Process:", pid, " ospid:", os.getpid(), " time:", time.time() - start_time, " END"
     lock.release()
+
 
 def multijobs(nprocesses):
     """ Do some parallel work by submitting
@@ -32,43 +39,44 @@ def multijobs(nprocesses):
 
     lock = Lock()
 
-    #mark the start time
-    startTime = time.time()
-     
-    #create a process Pool with N processes
+    # mark the start time
+    start_time = time.time()
+
+    # create a process Pool with N processes
     #pool = Pool(processes=nprocesses)
 
     print "---- Prepare all the jobs ----"
     # Put all the jobs to be done in a queue
     jobs_q = Queue()
     jobs = []
-    for p in range(nprocesses):
-         job = Process(target=doWork, args=(int(random.random()), p, lock))
-         jobs_q.put(job)
-         jobs.append(job)
+    for process in range(nprocesses):
+        job = Process(target=do_work, args=(int(random.random()), process, lock))
+        jobs_q.put(job)
+        jobs.append(job)
 
     # Batch mode
-    print "---- Launch jobs ----"
+    print "---- Launch " + str(nprocesses) +" jobs ----"
     jobs_started_q = Queue()
     # Go as long as there are jobs to do
     while not jobs_q.empty():
-         job = jobs_q.get()
-         job.start()
-         jobs_started_q.put(job)
+        job = jobs_q.get()
+        job.start()
+        jobs_started_q.put(job)
 
     # Block master until all started jobs are done
     while not jobs_started_q.empty():
         job = jobs_started_q.get()
-        job.join() # join: Block the calling thread (here master) until the job is finished
-             
-    #mark the end time
-    endTime = time.time()
-    #calculate the total time it took to complete the work
-    workTime =  endTime - startTime
-     
-    #print some infos
+        job.join()  # join: Block the calling thread (here master) until the job is finished
+
+    # mark the end time
+    end_time = time.time()
+    # calculate the total time it took to complete the work
+    work_time = end_time - start_time
+
+    # print some infos
     print "---- Master ----"
-    print "The job took " + str(workTime) + " seconds to complete"
+    print "The job took " + str(work_time) + " seconds to complete"
+
 
 def multijobs_batch(nprocesses, batch_size):
     """ Do some parallel work by submitting
@@ -80,51 +88,53 @@ def multijobs_batch(nprocesses, batch_size):
 
     lock = Lock()
 
-    #mark the start time
-    startTime = time.time()
-     
-    #create a process Pool with N processes
+    # mark the start time
+    start_time = time.time()
+
+    # create a process Pool with N processes
     #pool = Pool(processes=nprocesses)
 
     print "---- Prepare all the jobs ----"
     # Put all the jobs to be done in a queue
     jobs_q = Queue()
     jobs = []
-    for p in range(nprocesses):
-         job = Process(target=doWork, args=(int(random.random()), p, lock))
-         jobs_q.put(job)
-         jobs.append(job)
+    for process in range(nprocesses):
+        job = Process(target=do_work, args=(int(random.random()), process, lock))
+        jobs_q.put(job)
+        jobs.append(job)
 
     # Batch mode
-    print "---- Launch jobs ----"
+    print "---- Launch " + str(nprocesses) +" jobs ----"
     jobs_started_q = Queue()
     # Go as long as there are jobs to do
     while not jobs_q.empty():
 
+        print " --> Launching a batch of max " + str(batch_size) + " jobs"
         # Launch jobs by batch and put launched jobs in another queue
         while not jobs_q.empty() and jobs_started_q.qsize() != batch_size:
-             job = jobs_q.get()
-             job.start()
-             jobs_started_q.put(job)
+            job = jobs_q.get()
+            job.start()
+            jobs_started_q.put(job)
 
+        print " --> Launched " + str(jobs_started_q.qsize()) + " jobs"
         # Block master until all started jobs are done
         while not jobs_started_q.empty():
             job = jobs_started_q.get()
-            job.join() # join: Block the calling thread (here master) until the job is finished
-             
-    #mark the end time
-    endTime = time.time()
-    #calculate the total time it took to complete the work
-    workTime =  endTime - startTime
-     
-    #print some infos
+            job.join()  # join: Block the calling thread (here master) until the job is finished
+
+    # mark the end time
+    end_time = time.time()
+    # calculate the total time it took to complete the work
+    work_time = end_time - start_time
+
+    # print some infos
     print "---- Master ----"
-    print "The job took " + str(workTime) + " seconds to complete"
+    print "The job took " + str(work_time) + " seconds to complete"
+
 
 if __name__ == '__main__':
-    ncpus = multiprocessing.cpu_count()
-    print "Available number of procs :", ncpus 
-    nprocesses = 10
-    print "Number of work processes  :", nprocesses
+    NCPUS = multiprocessing.cpu_count()
+    print "Available number of procs :", NCPUS
+    NPROCESSES = 10
 
-    multijobs_batch(nprocesses, batch_size = ncpus)
+    multijobs_batch(NPROCESSES, batch_size=NCPUS)
